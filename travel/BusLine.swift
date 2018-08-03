08d5c361f40015c4ad5b534335e2da043ed36e08
Patrick Weaver <pjpwea@gmail.com>
2018-08-02 21:02:32 -0400

    Working API call and parsing JSON

--- travel/BusLine.swift
+++ travel/BusLine.swift
@@ -19,3 +19,54 @@ struct BusLineVariant {
     let origin: BusStop
     let destination: BusStop
 }
+
+
+
+
+/* Json Parsing */
+
+struct BusDiscoveryBlob: Decodable {
+    let code: Int?
+    let currentTime: Int?
+    let data: BusDiscoveryData?
+    let text: String?
+    let version: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case code
+        case currentTime
+        case data
+        case text
+        case version
+    }
+}
+
+struct BusDiscoveryData: Decodable {
+    let limitedExceeded: Bool?
+    let list: [BusDiscoveryLine]?
+}
+
+struct BusDiscoveryLine: Decodable {
+    let agencyId: String?
+    let color: String?
+    let description: String?
+    let id: String?
+    let longName: String?
+    let shortName: String?
+    let textColor: String?
+    let type: Int?
+    let url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case agencyId
+        case color
+        case description
+        case id
+        case longName
+        case shortName
+        case textColor
+        case type
+        case url
+    }
+}
+
